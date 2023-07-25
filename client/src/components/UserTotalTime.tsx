e63e3c58fbb7bb162986d79e6340efc281b8c6ce
RMerkulov <fotoromario@gmail.com>
2023-07-25 11:05:41 +0300

    upd

--- client/src/components/UserTotalTime.tsx
+++ client/src/components/UserTotalTime.tsx
@@ -4,6 +4,7 @@ import { setUserHeader } from "../utils/axios-utils";
 import useGetTotalTime from "../hooks/useGetTotalTime";
 import toast from "react-hot-toast";
 import { currentDateIntervals, formatTime } from "../helpers/times";
+import { BiSolidCircle } from "react-icons/bi";
 
 const UserTotalTime = () => {
   const currentDate = new Date().toISOString().slice(0, 10);
@@ -40,12 +41,18 @@ const UserTotalTime = () => {
   return (
     <div className="rounded-xl p-1">
       <h2 className="text-2xl text-amber-100">Time Intervals:</h2>
-      <ul className=" h-[100px] overflow-auto rounded-xl border-[1px] border-amber-100 border-opacity-95 py-1 px-2 mt-2">
+      <ul className=" h-[200px] overflow-auto rounded-xl border-[1px] border-amber-100 border-opacity-95 py-2 px-3 mt-2 ">
         {dayIntervals.map((interval) => (
-          <li key={interval._id} className="flex gap-3">
-            <span>{interval.description}</span>
-            <span>Start: {formatTime(new Date(interval.startTime))}</span>
-            <span>End: {formatTime(new Date(interval.endTime))}</span>
+          <li key={interval._id} className="flex items-start justify-between">
+            <span className="w-1/3 text-amber-100">{interval.description}</span>
+            <span className="text-orange-300 flex items-center justify-center gap-2">
+              <BiSolidCircle className="text-green-300" />{" "}
+              {formatTime(new Date(interval.startTime))}
+            </span>
+            <span className="text-orange-300 flex items-center justify-center gap-2">
+              <BiSolidCircle className="text-red-400" />{" "}
+              {formatTime(new Date(interval.endTime))}
+            </span>
           </li>
         ))}
       </ul>
