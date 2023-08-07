a330e144db784526a6dbf81003d987086e10a910
RMerkulov <fotoromario@gmail.com>
2023-08-07 11:09:48 +0300

    test_1

--- server/api/index.js
+++ server/api/index.js
@@ -1,20 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import {
-  getAllUsers,
-  getMe,
-  login,
-  register,
-} from "../controllers/UserController.js";
-import handleValidationErrors from "../utils/handleValidationErrors.js";
-import checkAuth from "../utils/checkAuth.js";
 import mongoose from "mongoose";
-import {
-  deleteTimeInterval,
-  getIntervals,
-  saveTimeInterval,
-} from "../controllers/TimeController.js";
 
 const app = express();
 
@@ -32,24 +19,24 @@ app.get("/api", (req, res) => {
 });
 
 // register user
-app.post(
-  "/api/auth/register",
-  // registerValidation,
-  handleValidationErrors,
-  register
-);
-//login user
-app.post("/api/auth/login", handleValidationErrors, login);
-//get user info
-app.get("/api/auth/me", checkAuth, getMe);
-//add time interval for a user
-app.post("/api/users/:userId/time-intervals", saveTimeInterval);
-//delete time intervals
-app.delete("/api/users/:userId/time-intervals", deleteTimeInterval);
-//get the total time of a user
-app.get("/api/users/:userId/time-intervals", getIntervals);
-//get all users
-app.get("/api/users", getAllUsers);
+// app.post(
+//   "/api/auth/register",
+//   registerValidation,
+//   handleValidationErrors,
+//   register
+// );
+// //login user
+// app.post("/api/auth/login", loginValidation, handleValidationErrors, login);
+// //get user info
+// app.get("/api/auth/me", checkAuth, getMe);
+// //add time interval for a user
+// app.post("/api/users/:userId/time-intervals", saveTimeInterval);
+// //delete time intervals
+// app.delete("/api/users/:userId/time-intervals", deleteTimeInterval);
+// //get the total time of a user
+// app.get("/api/users/:userId/time-intervals", getIntervals);
+// //get all users
+// app.get("/api/users", getAllUsers);
 
 //CONNECT TO DB
 mongoose
