f54ed5bd65bd98d22d466fd9f22d96ceabc55151
RMerkulov <fotoromario@gmail.com>
2023-08-07 11:41:34 +0300

    upd port

--- client/src/utils/axios-utils.ts
+++ client/src/utils/axios-utils.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3004";
+const BASE_URL = "https://clock-work-server.vercel.app";
 
 const apiClient = axios.create({ baseURL: BASE_URL });
 
