88135ffbbdba74e45c2260e0ec8babdfaf817d70
RMerkulov <fotoromario@gmail.com>
2023-08-07 11:22:48 +0300

    test_4

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
-import { saveTimeInterval } from "../controllers/TimeController.js";
+import {
+  deleteTimeInterval,
+  getIntervals,
+  saveTimeInterval,
+} from "../controllers/TimeController.js";
+import handleValidationErrors from "../utils/handleValidationErrors.js";
+import {
+  loginValidation,
+  registerValidation,
+} from "../validations/validation.js";
+import {
+  getAllUsers,
+  getMe,
+  login,
+  register,
+} from "../controllers/UserController.js";
+import checkAuth from "../utils/checkAuth.js";
 
 const app = express();
 
@@ -20,24 +36,24 @@ app.get("/api", (req, res) => {
 });
 
 // register user
-// app.post(
-//   "/api/auth/register",
-//   registerValidation,
-//   handleValidationErrors,
-//   register
-// );
+app.post(
+  "/api/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  register
+);
 // //login user
-// app.post("/api/auth/login", loginValidation, handleValidationErrors, login);
+app.post("/api/auth/login", loginValidation, handleValidationErrors, login);
 // //get user info
-// app.get("/api/auth/me", checkAuth, getMe);
+app.get("/api/auth/me", checkAuth, getMe);
 // //add time interval for a user
 app.post("/api/users/:userId/time-intervals", saveTimeInterval);
 // //delete time intervals
-// app.delete("/api/users/:userId/time-intervals", deleteTimeInterval);
+app.delete("/api/users/:userId/time-intervals", deleteTimeInterval);
 // //get the total time of a user
-// app.get("/api/users/:userId/time-intervals", getIntervals);
+app.get("/api/users/:userId/time-intervals", getIntervals);
 // //get all users
-// app.get("/api/users", getAllUsers);
+app.get("/api/users", getAllUsers);
 
 //CONNECT TO DB
 mongoose
