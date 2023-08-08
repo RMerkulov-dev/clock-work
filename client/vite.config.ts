43651fb97af0eedb6be4834cdcb8573209f90371
RMerkulov <fotoromario@gmail.com>
2023-08-08 17:51:51 +0300

    add new config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "url";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "nested/index.html"),
+      },
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000,
