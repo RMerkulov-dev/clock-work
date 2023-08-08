663bff6f6716b3454c83e769cf0f90606c6cb88f
RMerkulov <fotoromario@gmail.com>
2023-08-08 13:50:03 +0300

    add mail_2

--- server/jobs/sendEmail.js
+++ server/jobs/sendEmail.js
@@ -20,8 +20,13 @@ async function sendWelcomeEmail(recipientEmail, fullName) {
     await transporter.sendMail({
       from: "clockworkactivity@outlook.com",
       to: recipientEmail,
-      subject: "Welcome to Clockwork Activity",
-      text: `Hello ${fullName}! Welcome to Clockwork Activity! Check your time and enjoy:).`,
+      subject: "Welcome to Clockwork App",
+      html: `
+        <p>Hi ${fullName}!</p>
+        <p>Welcome to Clockwork Activity!</p>
+        <p>This cool app was made to train my skills in the MERN stack, and I hope you enjoy it.</p>
+        <p>To get to know me, go to my <a href="https://roman-merkulov-resume-2.netlify.app/">portfolio</a>.</p>
+      `,
     });
   } catch (err) {
     console.error("Error sending email:", err);
