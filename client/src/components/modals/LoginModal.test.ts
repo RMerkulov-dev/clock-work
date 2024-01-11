86c1b2f3a24c9f5c100310af0b9ff06e587bc02e
RMerkulov <fotoromario@gmail.com>
2024-01-11 13:18:50 +0200

    upd

--- client/src/components/modals/LoginModal.test.ts
+++ client/src/components/modals/LoginModal.test.ts
@@ -1,10 +1,42 @@
-import {describe} from "vitest";
-import {useRegisterModal} from "../../hooks/useRegisterModal";
-import {renderHook} from '@testing-library/react'
+import { describe, expect, vi } from "vitest";
+import { useRegisterModal } from "../../hooks/useRegisterModal";
+import {act, render, renderHook} from "@testing-library/react";
+import LoginModal from "./LoginModal";
 
+vi.mock("react-router-dom", () => ({
+  useNavigate: vi.fn(),
+}));
+vi.mock("path to hooks");
+vi.mock("path to axios utils");
+vi.mock("react-hot-toast");
 
-describe("Test Login Modal component",()=>{
-    it('Is useRegister hook initial state closed',()=>{
-const {result}=renderHook(()=>useRegisterModal())
+const setup = () => {
+    render( <LoginModal />);
+};
+
+describe("Test Login Modal component", () => {
+  it("Is useRegister hook initial state closed", () => {
+    const { result } = renderHook(() => useRegisterModal());
+    expect(result.current.isOpen).toBe(false);
+  });
+
+  it("Should open modal when isOpen called", () => {
+    const { result } = renderHook(() => useRegisterModal());
+
+    act(() => {
+      result.current.onOpen();
+    });
+
+    act(() => {
+      result.current.onClose();
+    });
+
+    expect(result.current.isOpen).toBe(false);
+  });
+});
+
+describe("Render LoginModal Component correctly",()=>{
+    it("rendering correctly",()=>{
+     setup()
     })
 })
