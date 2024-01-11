a6b4dd7664a05426e42ab19e6e4b1027a8a917a2
RMerkulov <fotoromario@gmail.com>
2024-01-11 11:32:44 +0200

    upd dep

--- client/src/components/modals/LoginModal.test.ts
+++ client/src/components/modals/LoginModal.test.ts
@@ -1,8 +1,10 @@
 import {describe} from "vitest";
+import {useRegisterModal} from "../../hooks/useRegisterModal";
+import {renderHook} from '@testing-library/react'
 
 
 describe("Test Login Modal component",()=>{
     it('Is useRegister hook initial state closed',()=>{
-
+const {result}=renderHook(()=>useRegisterModal())
     })
 })
